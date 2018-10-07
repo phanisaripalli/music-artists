2d234b581e4966c8dd2ba1e4cf0bcc7eac8f8282
Phani <saripalli.phani@gmail.com>
2018-10-07 13:30:21 +0200

    removed unused imports

--- src/test/java/com/saripalli/musicartists/controller/ArtistControllerTest.java
+++ src/test/java/com/saripalli/musicartists/controller/ArtistControllerTest.java
@@ -1,29 +1,16 @@
 package com.saripalli.musicartists.controller;
 
-import static org.junit.Assert.assertTrue;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import com.saripalli.musicartists.apiclient.ArtistClient;
-import com.saripalli.musicartists.apiclient.CoverArtArchiveClient;
-import com.saripalli.musicartists.apiclient.DiscogsClient;
-import com.saripalli.musicartists.apiclient.MusicBrainzClient;
-import com.saripalli.musicartists.models.BaseModel;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
